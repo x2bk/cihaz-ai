c31065358133561c9d4f6d1c3f2cf0c41a532c68
x2bk <bera.kahraman@hotmail.com>
2025-07-12 20:10:42 +0300

    Temizlik, v1 embedding hatası düzeltildi, gereksiz android dizinleri silindi, ana proje güncellendi

--- cihaz_ai_clean/android/app/src/main/kotlin/com/example/cihaz_ai_clean/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-package com.example.cihaz_ai_clean
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity : FlutterActivity()
--- cihaz_ai_final/android/app/src/main/kotlin/com/example/cihaz_ai_final/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cihaz_ai_final
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
